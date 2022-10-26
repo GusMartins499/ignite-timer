246ca89d13dcf4a1c25f608d4913534a38994839
Gustavo <gustavosm994@gmail.com>
2022-10-26 19:37:00 -0300

    fix(component): remove button component

--- src/components/Button/button.styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonContainer = styled.button`
-  width: 160px;
-  height: 80px;
-  background-color: ${(props) => props.theme['green-500']};
-`
