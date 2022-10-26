d1f1282e57c0ec60cab122d037f50c5a9e3cb7ca
Gustavo <gustavosm994@gmail.com>
2022-10-26 19:45:29 -0300

    feat(react-router-dom): outlet react router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom'
 
 import { History } from '@/pages/history'
 import { Home } from '@/pages/home'
+import { DefaultLayout } from '@/layouts/default'
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
